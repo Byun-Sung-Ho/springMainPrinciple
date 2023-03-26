d6fb7127a776a7fa1ac67071154f0f733735ef7a
Byun-Sung-Ho <bsh4237@gmail.com>
2023-03-26 23:03:29 +0900

    make example

---
+++ src/main/java/hello/core/discount/FixedDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixedDiscountPolicy implements DiscountPolicy {
+
+    private int fixedAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return fixedAmount;
+        } else {
+            return 0;
+        }
+    }
+}
