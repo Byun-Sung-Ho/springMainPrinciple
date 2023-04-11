daf9ebb98b1846297dde2639863f5f3ec6b369fc
Byun-Sung-Ho <bsh4237@gmail.com>
2023-04-11 17:51:04 +0900

    sigleton end and Role of Configuration

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+        MemberServiceImpl bean1 = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl bean2 = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemberRepository memberRepository1 = bean1.getMemberRepository();
+        MemberRepository memberRepository2 = bean2.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+
+    }
+    @Test
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+    }
+}
